4cb0ab34720b078df1283d8e67634a7bb7ee8524
MosaicMRI <mosaicmri@gmail.com>
2026-03-13 17:35:40 -0700

    Add benchmark email verification flow

--- benchmark/config.js
+++ benchmark/config.js
@@ -1,4 +1,5 @@
 window.BENCHMARK_API_CONFIG = {
+  apiBaseUrl: "https://6zbapch72kqdqefv2ocgpdbwsm0nrfoj.lambda-url.us-east-1.on.aws",
   primaryMetric: "PSNR",
   maxUploadBytes: 8589934592,
   dailyLimit: 1,
