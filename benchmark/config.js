5214d65872db4999e36cc22d7de6d7bcf1ffc377
MosaicMRI <mosaicmri@gmail.com>
2026-04-06 14:52:45 -0700

    Temporarily disable public site

--- benchmark/config.js
+++
@@ -1,7 +0,0 @@
-window.BENCHMARK_API_CONFIG = {
-  apiBaseUrl: "https://6zbapch72kqdqefv2ocgpdbwsm0nrfoj.lambda-url.us-east-1.on.aws",
-  primaryMetric: "PSNR",
-  maxUploadBytes: 8589934592,
-  dailyLimit: 1,
-  pollMs: 7000
-};
